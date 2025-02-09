a348478c7467bfe41da5126e66e5f050d61ac32a
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2025-02-09 16:31:25 +0100

    Setup

---
+++ src/katas/mars-rover/MarsRover.spec.ts
@@ -0,0 +1,9 @@
+import { MarsRover } from './MarsRover';
+
+describe('Mars Rover', () => {
+  let marsRover: MarsRover;
+
+  beforeEach(() => marsRover = new MarsRover())
+
+  it('works!', () => expect(marsRover).toBeTruthy())
+})
