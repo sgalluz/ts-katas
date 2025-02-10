aba9f1242cbd9b7483267949c30bb668f784353c
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2025-02-10 23:30:36 +0100

    Obstacles: refactoring

--- src/katas/mars-rover/MarsRover.ts
+++ src/katas/mars-rover/MarsRover.ts
@@ -9,7 +9,7 @@ export class MarsRover {
   private direction: Direction = new North();
   private coordinate: Coordinate = new Coordinate(0, 0)
 
-  execute(commands: string) {
+  execute(commands: string): string {
     let hasObstacle = false
     for (const command of commands) {
       if (command === 'R') {
@@ -19,15 +19,17 @@ export class MarsRover {
         this.direction = this.direction.left();
       }
       if (command === 'M') {
-        const coordinate = this.grid.moveToNextCoordinateFrom(this.coordinate, this.direction.value);
-        if (!coordinate) {
-          hasObstacle = true;
-        } else {
-          hasObstacle = false;
-          this.coordinate = coordinate;
-        }
+        hasObstacle = this.move();
       }
     }
-    return (hasObstacle ? 'O:' : '') + `${this.coordinate.x}:${this.coordinate.y}:${this.direction.value}`;
+
+    const obstaclePrefix = hasObstacle ? 'O:' : '';
+    return `${obstaclePrefix}${this.coordinate.x}:${this.coordinate.y}:${this.direction.value}`;
+  }
+
+  private move(): boolean {
+    const coordinate = this.grid.moveToNextCoordinateFrom(this.coordinate, this.direction.value);
+    this.coordinate = coordinate ?? this.coordinate;
+    return !coordinate || false;
   }
 }
