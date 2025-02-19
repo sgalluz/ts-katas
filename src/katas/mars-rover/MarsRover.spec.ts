5d0de7a999916a15232867ed4b8358eb41af658c
Agostino Marzotta <agostino.marzotta@doctolib.com>
2025-02-19 15:49:06 +0100

    WIP

--- src/katas/mars-rover/MarsRover.spec.ts
+++ src/katas/mars-rover/MarsRover.spec.ts
@@ -1,9 +1,13 @@
-import { MarsRover } from './MarsRover';
+import { navigate } from './MarsRover';
 
 describe('Mars Rover', () => {
-  let marsRover: MarsRover;
-
-  beforeEach(() => marsRover = new MarsRover())
-
-  it('works!', () => expect(marsRover).toBeTruthy())
+  it('No command', () => expect(navigate('')).toBe('0:0:N'))
+  it('Rotate Left 1 times', () => expect(navigate('L')).toBe('0:0:W'))
+  it('Rotate Right 1 times', () => expect(navigate('R')).toBe('0:0:E'))
+  it('Rotate Right 2 times', () => expect(navigate('RR')).toBe('0:0:S'))
+  it('Rotate Right 3 times', () => expect(navigate('RRR')).toBe('0:0:W'))
+  it('Rotate Right 4 times', () => expect(navigate('RRRR')).toBe('0:0:N'))
+  it('Move 1 times', () => expect(navigate('M')).toBe('0:1:N'))
+  it('Move 1 times south', () => expect(navigate('RRM')).toBe('0:9:S'))
+  it('Move 10 times', () => expect(navigate('MMMMMMMMMM')).toBe('0:0:N'))
 })
--- src/katas/mars-rover/MarsRover.ts
+++ src/katas/mars-rover/MarsRover.ts
@@ -1,4 +1,65 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class MarsRover {
-  // TODO: implement me!
+
+export type Position = [number, number]
+
+export type Orientation = 'N' | 'E' | 'S' | 'W'
+
+export type Command = 'R' | 'L' | 'M'
+
+
+
+export interface State {
+    position: Position
+    orientation: Orientation
+}
+
+export type Transformation = (state: State) => State
+export function navigate(commands: string): string {
+  const initialState : State = { position: [0, 0], orientation: 'N' }
+  const commandList: Command[] = commands.split('') as Command[]
+  const state = commandList.reduce((acc, curr) => executors[curr](acc), initialState)
+  return printState(state)
+}
+
+function printState(state: State): string {
+  return `${state.position[0]}:${state.position[1]}:${state.orientation}`
+}
+
+
+function rotate(state: State, direction: Command): State {
+  const orientations : Orientation[] = ['N', 'E', 'S', 'W']
+  const currentIndex = orientations.indexOf(state.orientation)
+  const nextIndex = direction === 'R' ? (currentIndex + 1) % 4 : (currentIndex + 3) % 4
+  return { ...state, orientation: orientations[nextIndex] }
+}
+
+function move(state: State): State {
+  const [x, y] = state.position
+  switch (state.orientation) {
+  case 'N':
+    return { ...state, position: [x, moveCursor(y, 'forward')] }
+  case 'E':
+    return { ...state, position: [moveCursor(x, 'forward'), y] }
+  case 'S':
+    return { ...state, position: [x, moveCursor(y, 'backward')] }
+  case 'W':
+    return { ...state, position: [ moveCursor(x, 'backward'), y] }
+  }
+}
+
+function moveCursor (x: number, sh: string){
+  if (sh === 'forward') {
+    return (x + 1) % 10
+  } else {
+    if (x === 0) {
+      return 9
+    } else {
+      return x - 1
+    }
+  }
+}
+
+const executors: Record<Command, Transformation> = {
+  'R' : (state: State ) => rotate( state, 'R'),
+  'L' : (state: State ) => rotate( state, 'L'),
+  'M' : (state: State ) => move( state )
 }
