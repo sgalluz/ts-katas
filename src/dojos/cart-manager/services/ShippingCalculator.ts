3d05ffbf952b2d39dcadb2aec0a6a42a7e2ca577
Stefano Galluzzi <stefano.galluzzi@doctolib.com>
2025-11-12 18:18:30 +0100

    refactor(SRP): create ShippingCalculator to be injected in CartManager

---
+++ src/dojos/cart-manager/services/ShippingCalculator.ts
@@ -0,0 +1,34 @@
+import { IShippingService } from './ShippingService'
+import { UserProfile, UserType } from '../models/UserProfile'
+
+export class ShippingCalculator {
+  private static readonly BASE_SHIPPING_FEE = 15
+  private static readonly EXTRA_FEE = 10
+
+  constructor(private readonly shippingService: IShippingService) {
+  }
+
+  // FIXME: this should be imprpoved as five parameters for a method is too many...
+  public calculateShipping(
+    totalAfterDiscount: number,
+    totalWeight: number,
+    address: string,
+    profile: UserProfile,
+    couponCode: string | null
+  ): number {
+    const shippingCost = this.getBaseShippingCost(totalAfterDiscount, address, totalWeight, couponCode)
+
+    const shouldApplyExtraFee = profile.type === UserType.Guest && totalAfterDiscount + shippingCost > 200
+    const extraFee = shouldApplyExtraFee ? ShippingCalculator.EXTRA_FEE : 0
+
+    return shippingCost + extraFee
+  }
+
+  private getBaseShippingCost(totalAfterDiscount: number, address: string, totalWeight: number, couponCode: string | null) {
+    if (totalAfterDiscount < 50 && address) {
+      return this.shippingService.calculate(address, totalWeight)
+    }
+
+    return couponCode === 'FREE_SHIPPING' || totalAfterDiscount >= 100 ? 0 : ShippingCalculator.BASE_SHIPPING_FEE
+  }
+}
