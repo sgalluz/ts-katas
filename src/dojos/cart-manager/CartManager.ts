0ba00743a11bb1cc5a18a522d3bd3fc38d50dd68
Stefano Galluzzi <stefano.galluzzi@doctolib.com>
2025-11-14 23:37:38 +0100

    refactor(CartSummaryService): consume new service in CartManager

--- src/dojos/cart-manager/CartManager.ts
+++ src/dojos/cart-manager/CartManager.ts
@@ -1,21 +1,19 @@
 import { UserProfile } from './models/UserProfile'
 import { ILogger } from './services/Logger'
-import { IDiscountCalculator } from './services/DiscountCalculator'
-import { IShippingCalculator } from './services/ShippingCalculator'
 import { INotifier } from './services/Notifier'
 import { ICartValidator } from './services/CartValidator'
 import { CartItems } from './models/CartItems'
 import { ProductRepository } from './repositories/ProductRepository'
 import { CartSummary } from './models/CartSummary'
 import { CheckoutOptions } from './models/CheckoutOptions'
+import { ICartSummaryService } from './services/CartSummaryService'
 
 export class CartManager {
   constructor(
         private readonly userProfile: UserProfile,
         private readonly cartItems: CartItems,
         private readonly productRepository: ProductRepository,
-        private readonly discountCalculator: IDiscountCalculator,
-        private readonly shippingCalculator: IShippingCalculator,
+        private readonly cartSummaryService: ICartSummaryService,
         private readonly cartValidator: ICartValidator,
         private readonly notifier: INotifier,
         private readonly logger: ILogger
@@ -36,33 +34,17 @@ export class CartManager {
     return { success: true, message }
   }
 
-  public getFinalSummary({ couponCode = null, shippingAddress = '' }: CheckoutOptions = {}): CartSummary {
-    const summary = this.generateSummary(couponCode, shippingAddress)
+  public getFinalSummary(checkoutOptions: CheckoutOptions = {}): CartSummary {
+    const summary = this.cartSummaryService.generateSummary(
+      this.cartItems,
+      this.userProfile,
+      checkoutOptions
+    )
 
     if (this.cartValidator.isHighValueOrder(summary.finalTotal, this.cartItems.length)) {
       this.notifier.sendHighValueOrderAlert(this.userProfile.id, summary.finalTotal)
     }
 
     return summary
-  }
-
-  private generateSummary(couponCode: string | null, shippingAddress: string) {
-    const total = this.cartItems.totalPrice
-
-    const discount = this.discountCalculator.calculateDiscount(total, this.userProfile, couponCode)
-
-    const totalAfterDiscount = total - discount
-
-    const shippingCost = this.shippingCalculator.calculateShipping({
-      totalPrice: totalAfterDiscount,
-      totalWeight: this.cartItems.totalWeight,
-      address: shippingAddress,
-      userProfile: this.userProfile,
-      couponCode
-    })
-
-    const finalTotal = totalAfterDiscount + shippingCost
-
-    return { total, discount, shippingCost, finalTotal }
   }
 }
