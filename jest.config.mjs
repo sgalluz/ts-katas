3c3dc8f822d6e3d7da0c4f78752a2e305a330a81
Stefano Galluzzi <14968717+sgalluz@users.noreply.github.com>
2025-11-15 15:18:23 +0100

    chore(repo): allow coverage report generation (#8)

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,25 @@ const config = {
   verbose: true,
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-  }
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'cobertura',
+    'clover',
+    'json',
+    'json-summary',
+    'lcov',
+    ['text', { skipFull: true }],
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 }
 
 export default config
