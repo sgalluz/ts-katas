c4711d42b0867ebe1a9815092e5e244b275adcfc
doctolorentz <lorenzo.fermi@doctolib.com>
2025-11-20 12:52:36 +0100

    Add constructor tests

--- src/dojos/cart-manager/CartManager.spec.ts
+++ src/dojos/cart-manager/CartManager.spec.ts
@@ -13,6 +13,44 @@ describe('Cart Manager', () => {
 
   beforeEach(() => {
     cartManager = new CartManager(userProfile)
+
+    jest.spyOn(console, 'log').mockImplementation(() => {
+      return (message: string) => {
+        return message
+      }
+    })
+  })
+
+  it('logs the cart initialization', () => {
+    // Act
+
+    new CartManager(userProfile)
+
+    // Assert  
+    expect(console.log).toHaveBeenCalledWith('[LOGGING] Cart initialized for user: 1.')
+    jest.restoreAllMocks()
+  })
+
+  it('for already present saved cart items in profile, loads them into the cart', () => {
+    // Act
+
+    new CartManager(userProfile)
+    expect(console.log).toHaveBeenCalledWith('[LOGGING] Cart initialized for user: 1.')
+
+    const filledUserProfile: UserProfile = {
+      id: 1,
+      type: UserType.Standard,
+      isFirstPurchase: true,
+      savedCartItems: [{ productId: 2, quantity: 3 }]
+    }
+
+    new CartManager(filledUserProfile)
+
+
+    // Assert
+    expect(console.log).toHaveBeenCalledWith('Loading 1 saved items for user 1')
+    expect(console.log).toHaveBeenCalledWith('[LOGGING] Cart initialized for user: 1.')
+
   })
 
   it('should get the correct first purchase discount', () => {
